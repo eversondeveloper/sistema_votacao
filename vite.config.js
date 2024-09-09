8d411db53c26f3aff9f57952e6538dbb3084210a
Everson33rj <eversonsilvadeveloper@gmail.com>
2024-09-09 13:50:31 -0300

    Configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Isso garante que os caminhos sejam relativos
+  base: '/sistema_votacao', // Isso garante que os caminhos sejam relativos
   plugins: [react()],
 });
